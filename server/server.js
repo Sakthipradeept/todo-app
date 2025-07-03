42a06adb81f437f9deaaa9f577874f5c7128e282
Sakthipradeept <167534074+Sakthipradeept@users.noreply.github.com>
2025-07-03 22:30:27 +0530

    new file

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const Task = require("./models/Task");
+
+app.get("/api/tasks", async (req, res) => {
+  const user = req.query.user;
+  const tasks = await Task.find({ createdBy: user });
+  res.json(tasks);
+});
+
+app.post("/api/tasks", async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
+
+app.delete("/api/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.log(err));
+app.put("/api/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update task" });
+  }
+});
